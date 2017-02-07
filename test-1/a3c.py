a403f311726294e14d3d151bec05043259e716a1
Max Lapan <max.lapan@gmail.com>
2017-02-07 22:07:31 +0100

    Test of mse error

--- test-1/a3c.py
+++ test-1/a3c.py
@@ -81,19 +81,19 @@ def make_model(state_shape, n_actions, train_mode=True):
     BETA = 0.01
     entropy_loss_t = Lambda(lambda p: BETA * K.sum(p * K.log(p)), output_shape=(1, ), name='entropy_loss')(policy_t)
 
-    def policy_loss_func(args):
-        policy_t, action_t = args
-        oh = K.one_hot(action_t, nb_classes=n_actions)
-        oh = K.squeeze(oh, 1)
-        p = K.log(policy_t) * oh
-        p = K.sum(p, axis=-1, keepdims=True)
-        return p * K.stop_gradient(value_t - reward_t)
-
-    policy_loss_t = merge([policy_t, action_t], mode=policy_loss_func,
-                          output_shape=(1, ), name='policy_loss')
+    # def policy_loss_func(args):
+    #     policy_t, action_t = args
+    #     oh = K.one_hot(action_t, nb_classes=n_actions)
+    #     oh = K.squeeze(oh, 1)
+    #     p = K.log(policy_t) * oh
+    #     p = K.sum(p, axis=-1, keepdims=True)
+    #     return p * K.stop_gradient(value_t - reward_t)
+    #
+    # policy_loss_t = merge([policy_t, action_t], mode=policy_loss_func,
+    #                       output_shape=(1, ), name='policy_loss')
 #    loss_t = merge([policy_loss_t, policy_loss_t], mode='ave', name='loss')
 
-    policy_model = Model(input=[in_t, reward_t, action_t], output=policy_loss_t)
+    policy_model = Model(input=[in_t, reward_t, action_t], output=policy_t)
     value_model = Model(input=[in_t, reward_t, action_t], output=value_loss_t)
     return run_model, policy_model, value_model
 
@@ -116,11 +116,17 @@ def create_batch(env, run_model, num_episodes, steps_limit=1000, gamma=1.0):
         while True:
             # chose action to take
             policy, value = run_model.predict_on_batch(np.array([state]))
+            policy = policy[0]
+            value = value[0]
+            print(policy, value)
             values.append(value)
             action = np.argmax(policy)
             next_state, reward, done, _ = env.step(action)
             sum_reward = gamma*sum_reward + reward
-            episodes.append((state, action, sum_reward))
+            policy_tgt = np.copy(policy)
+            print(policy, policy_tgt, action)
+            policy_tgt[action] = sum_reward
+            episodes.append((state, action, sum_reward, policy_tgt))
             state = next_state
             step += 1
             if done or (steps_limit is not None and steps_limit == step):
@@ -129,7 +135,8 @@ def create_batch(env, run_model, num_episodes, steps_limit=1000, gamma=1.0):
     logger.info("Mean final reward: %.3f, max: %.3f, value: %s", np.mean(rewards), np.max(rewards), np.mean(values))
     # convert data to train format
     np.random.shuffle(episodes)
-    return list(map(np.array, zip(*episodes)))
+    data = list(map(np.array, zip(*episodes)))
+    return data[:-1], data[-1]
 
 
 if __name__ == "__main__":
@@ -147,7 +154,7 @@ if __name__ == "__main__":
     run_m, policy_m, value_m = make_model(state_shape, n_actions, train_mode=True)
     policy_m.summary()
 
-    policy_m.compile(optimizer=Adagrad(), loss={'policy_loss': lambda a, b: b})
+    policy_m.compile(optimizer=Adagrad(), loss='mse')
     value_m.compile(optimizer=Adagrad(), loss={'value_loss': lambda a, b: b})
 
     # test run, to check correctness
@@ -165,10 +172,10 @@ if __name__ == "__main__":
         step_limit = None
 
     for iter in range(100):
-        batch = create_batch(env, run_m, num_episodes=100, steps_limit=step_limit)
+        batch, policy_y = create_batch(env, run_m, num_episodes=100, steps_limit=step_limit)
         fake_y = np.zeros(shape=(len(batch[2]),))
-        policy_loss = policy_m.train_on_batch(batch, fake_y)
-        value_loss = value_m.train_on_batch(batch, fake_y)
+        policy_loss = policy_m.train_on_batch(batch, policy_y)
+        #value_loss = value_m.train_on_batch(batch, fake_y)
 #        value_loss = 0.0
         logger.info("%d: policy_loss: %s, value_loss: %s", iter, policy_loss, value_loss)
     pass
