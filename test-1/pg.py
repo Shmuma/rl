edbffa792df1d8bd98600dc6be605ba7ba6981db
Max Lapan <maxl@fornova.net>
2017-02-09 16:03:20 +0100

    Gradients are there, but no convergence :)

--- test-1/pg.py
+++ test-1/pg.py
@@ -13,10 +13,11 @@ logger.setLevel(logging.INFO)
 import gym, gym.wrappers
 
 from keras.models import Model
-from keras.layers import Input, Dense, Flatten
-from keras.optimizers import Adagrad, RMSprop
+from keras.layers import Input, Dense, Flatten, Lambda
+from keras.optimizers import Adagrad
+from keras import backend as K
 
-HISTORY_STEPS = 4
+HISTORY_STEPS = 2
 SIMPLE_L1_SIZE = 50
 SIMPLE_L2_SIZE = 50
 
@@ -30,12 +31,25 @@ def make_env(env_name, monitor_dir):
 
 def make_model(state_shape, n_actions):
     in_t = Input(shape=(HISTORY_STEPS,) + state_shape, name='input')
+    action_t = Input(shape=(1,), dtype='int32', name='action')
+    advantage_t = Input(shape=(1,), name='advantage')
+
     fl_t = Flatten(name='flat')(in_t)
     l1_t = Dense(SIMPLE_L1_SIZE, activation='relu', name='l1')(fl_t)
     l2_t = Dense(SIMPLE_L2_SIZE, activation='relu', name='l2')(l1_t)
-    value_t = Dense(n_actions, name='value', activation='softmax')(l2_t)
+    policy_t = Dense(n_actions, name='policy', activation='softmax')(l2_t)
 
-    return Model(input=in_t, output=value_t)
+    def loss_func(args):
+        p_t, act_t, adv_t = args
+        oh_t = K.one_hot(act_t, n_actions)
+        oh_t = K.squeeze(oh_t, 1)
+        p_oh_t = K.clip(K.log(K.sum(oh_t * p_t, axis=-1, keepdims=True)), -2.0, 2.0)
+        res_t = adv_t * p_oh_t
+        return res_t
+
+    loss_t = Lambda(loss_func, output_shape=(1,), name='loss')([policy_t, action_t, advantage_t])
+
+    return Model(input=[in_t, action_t, advantage_t], output=[policy_t, loss_t])
 
 
 def create_batch(iter_no, env, run_model, num_episodes, steps_limit=1000, gamma=1.0, tau=0.20, min_samples=None):
@@ -58,7 +72,11 @@ def create_batch(iter_no, env, run_model, num_episodes, steps_limit=1000, gamma=
         episode = []
         while True:
             # chose action to take
-            probs = run_model.predict_on_batch(np.array([state]))[0]
+            probs = run_model.predict_on_batch([
+                np.array([state]),
+                np.array([0]),
+                np.array([0.0])
+            ])[0][0]
             if np.random.random() < tau:
                 action = np.random.randint(0, len(probs))
             else:
@@ -82,13 +100,15 @@ def create_batch(iter_no, env, run_model, num_episodes, steps_limit=1000, gamma=
             break
 
     # mean_final_reward = np.mean(rewards)
+    # norm = np.linalg.norm(rewards)
+    # if norm < 1e-5:
+    #     norm = 1.0
     # disp = np.max(rewards) - np.min(rewards)
     # convert episodes into samples
     for episode, episode_reward in zip(episodes, rewards):
         # now we need to unroll our episode backward to generate training samples
         for state, probs, action, reward in reversed(episode):
-            target = episode_reward * np.log(probs[action])
-            samples.append((state, target))
+            samples.append((state, action, episode_reward))
 
     logger.info("%d: Have %d samples from %d episodes, mean final reward: %.3f, max: %.3f",
                 iter_no, len(samples), len(episodes), np.mean(rewards), np.max(rewards))
@@ -97,15 +117,18 @@ def create_batch(iter_no, env, run_model, num_episodes, steps_limit=1000, gamma=
     return list(map(np.array, zip(*samples)))
 
 
-def test_loss(y_true, y_pred):
-    return y_true + y_pred*0.0
+def create_fake_target(n_actions, batch_len):
+    return [
+        np.array([[0.0] * n_actions] * batch_len),
+        np.array([0.0] * batch_len)
+    ]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--env", default="CartPole-v0", help="Environment name to use")
     parser.add_argument("-m", "--monitor", help="Enable monitor and save data into provided dir, default=disabled")
-    parser.add_argument("-t", "--tau", type=float, default=0.2, help="Ratio of random steps, default=0.2")
+    parser.add_argument("-t", "--tau", type=float, default=0.0, help="Ratio of random steps, default=0.2")
     parser.add_argument("-i", "--iters", type=int, default=100, help="Count if iterations to take, default=100")
     args = parser.parse_args()
 
@@ -118,35 +141,31 @@ if __name__ == "__main__":
     model = make_model(state_shape, n_actions)
     model.summary()
 
-    model.compile(optimizer=Adagrad(), loss=test_loss)
+    loss_dict = {
+        # our model already outputs loss, so just take it as-is
+        'loss': lambda y_true, y_pred: y_pred,
+        # this will make zero gradients contribution
+        'policy': lambda y_true, y_pred: y_true
+    }
+
+    model.compile(optimizer=Adagrad(), loss=loss_dict)
 
     # gradient check
     if True:
-        batch, target_y = create_batch(0, env, model, tau=0, num_episodes=1, steps_limit=10, min_samples=None)
-        r = model.predict_on_batch(batch)
-        l = model.train_on_batch(batch, target_y)
-        r2 = model.predict_on_batch(batch)
+        batch, action, advantage = create_batch(0, env, model, tau=0, num_episodes=1, steps_limit=10, min_samples=None)
+        r = model.predict_on_batch([batch, action, advantage])
+        fake_out = create_fake_target(n_actions, len(batch))
+        l = model.train_on_batch([batch, action, advantage], fake_out)
+        r2 = model.predict_on_batch([batch, action, advantage])
+        logger.info("Test fit, mean loss: %s -> %s", np.mean(r[1]), np.mean(r2[1]))
 
-
-    epoch_limit = 2
     step_limit = 300
     if args.monitor is not None:
         step_limit = None
 
     for iter in range(args.iters):
-        batch, target_y = create_batch(iter, env, model, tau=args.tau,
-                                       num_episodes=10, steps_limit=step_limit, min_samples=500)
-        # iterate until our losses decreased 10 times or epoches limit exceeded
-        start_loss = None
-        loss = None
-        converged = False
-        for epoch in range(epoch_limit):
-            p_h = model.fit(batch, target_y, verbose=0, batch_size=128)
-            loss = np.min(p_h.history['loss'])
-
-            if start_loss is None:
-                start_loss = np.max(p_h.history['loss'])
-            else:
-                if start_loss / loss > 2:
-                    break
+        batch, action, advantage = create_batch(iter, env, model, tau=args.tau, num_episodes=10,
+                                                steps_limit=step_limit, min_samples=2000)
+        fake_out = create_fake_target(n_actions, len(batch))
+        model.fit([batch, action, advantage], fake_out, verbose=0, batch_size=512)
     pass
