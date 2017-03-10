b3e3f37c38ff1d9df9717150788669bd6465f942
Max Lapan <max.lapan@gmail.com>
2017-03-10 16:08:05 +0100

    Do not need it

--- test-1/tools/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ITER=138
-./a3c_atari_play.py -r ../logs/breakout/model-${ITER}000.h5 -e Breakout-v0 -m break-${ITER}k -v
-./a3c_atari_play.py -r ../logs/pong/model-${ITER}000.h5 -e Pong-v0 -m pong-${ITER}k -v
-./a3c_atari_play.py -r ../logs/river/model-${ITER}000.h5 -e Riverraid-v0 -m river-${ITER}k -v
