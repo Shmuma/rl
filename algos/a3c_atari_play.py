e3f60cf777ed62013b18b9362c5a00aa4094369c
Max Lapan <max.lapan@gmail.com>
2017-03-14 06:43:43 +0100

    Reflect latest changes

--- algos/a3c_atari_play.py
+++ algos/a3c_atari_play.py
@@ -2,18 +2,14 @@
 import argparse
 import numpy as np
 
-from keras.models import Model
-
-from a3c_atari import make_model, preprocess
-
 from algo_lib.common import make_env
-from algo_lib.atari_opts import HISTORY_STEPS, net_input
+from algo_lib.atari_opts import HISTORY_STEPS, net_input, preprocess_state
 from algo_lib.a3c import make_run_model
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--model", help="Model file to read. If not specified, will play randomly")
+    parser.add_argument("-r", "--model", required=True, help="Model file to read")
     parser.add_argument("-e", "--env", required=True, help="Environment to use")
     parser.add_argument("-m", "--monitor", help="Enable monitor and write to directory, default=disabled")
     parser.add_argument("--iters", type=int, default=100, help="Episodes to play, default=100")
@@ -24,13 +20,10 @@ if __name__ == "__main__":
     state_shape = env.observation_space.shape
     n_actions = env.action_space.n
 
-    if args.model is not None:
-        input_t, conv_out_t = net_input(state_shape)
-        model = make_run_model(input_t, conv_out_t, n_actions)
-        model.summary()
-        model.load_weights(args.model)
-    else:
-        model = None
+    input_t, conv_out_t = net_input()
+    model = make_run_model(input_t, conv_out_t, n_actions)
+    model.summary()
+    model.load_weights(args.model)
 
     rewards = []
     steps = []
@@ -40,15 +33,12 @@ if __name__ == "__main__":
         sum_reward = 0.0
         step = 0
         while True:
-            if model is None:
-                action = env.action_space.sample()
-            else:
-                probs, value = model.predict_on_batch([
-                    np.array([state]),
-                ])
-                probs, value = probs[0], value[0][0]
-                # take action
-                action = np.random.choice(len(probs), p=probs)
+            probs, value = model.predict_on_batch([
+                np.array([preprocess_state(state)]),
+            ])
+            probs, value = probs[0], value[0][0]
+            # take action
+            action = np.random.choice(len(probs), p=probs)
             state, reward, done, _ = env.step(action)
             step += 1
             sum_reward += reward
