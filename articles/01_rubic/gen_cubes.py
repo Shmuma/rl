a9e95fa6bce1945b042aff0d8603f94bc354872c
Max Lapan <max.lapan@gmail.com>
2018-10-19 09:40:20 +0300

    cleanup

--- articles/01_rubic/gen_cubes.py
+++ articles/01_rubic/gen_cubes.py
@@ -32,5 +32,3 @@ if __name__ == "__main__":
                 path.append(a.value)
                 s = cube_env.transform(s, a)
             fd_out.write(",".join(map(str, path)) + "\n")
-
-    pass
